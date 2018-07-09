0d5261e39253a325dbedf0ce9f764d043c383364
Stefan Gabos <contact@stefangabos.ro>
2018-07-09 21:47:28 +0300

    Updated version number

--- src/zebra_dialog.src.js
+++ src/zebra_dialog.src.js
@@ -21,7 +21,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Dialog/ here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.5 (last revision: July 05, 2018)
+ *  @version    1.6 (last revision: July 09, 2018)
  *  @copyright  (c) 2011 - 2018 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Dialog
