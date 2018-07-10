24dce68a099510bb6272239cf61a2ba1f8dbafa1
Stefan Gabos <contact@stefangabos.ro>
2018-07-10 10:10:49 +0300

    Updated examples

--- examples/examples.js
+++ examples/examples.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
     $('#example1').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('<strong>Zebra_Dialog</strong>, a small, compact and highly configurable dialog box plugin for jQuery');
+        new $.Zebra_Dialog('<strong>Zebra_Dialog</strong>, a small, compact and highly configurable dialog box plugin for jQuery');
     });
 
     $('#example2_1').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('Use error messages to let the user know that an action has not completed successfully and show the reason why that happened.', {
+        new $.Zebra_Dialog('Use error messages to let the user know that an action has not completed successfully and show the reason why that happened.', {
             type: 'error',
             title: 'Error'
         });
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
     $('#example2_2').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('Show warning messages like this when you are about to perform a sensitive operation and you want to make sure that the user understands the implications.', {
+        new $.Zebra_Dialog('Show warning messages like this when you are about to perform a sensitive operation and you want to make sure that the user understands the implications.', {
             type: 'warning',
             title: 'Warning'
         });
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
     $('#example2_3').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('Show confirmation dialogs like this when you want the user\'s accept for non-critical actions.', {
+        new $.Zebra_Dialog('Show confirmation dialogs like this when you want the user\'s accept for non-critical actions.', {
             type: 'question',
             title: 'Question'
         });
@@ -31,7 +31,7 @@ $(document).ready(function() {
 
     $('#example2_4').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('Use information boxes to show information or help messages that the user requested.', {
+        new $.Zebra_Dialog('Use information boxes to show information or help messages that the user requested.', {
             type: 'information',
             title: 'Information'
         });
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
     $('#example2_5').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('Use confirmation messages to let the user know that an action has completed successfully.', {
+        new $.Zebra_Dialog('Use confirmation messages to let the user know that an action has completed successfully.', {
             type: 'confirmation',
             title: 'Confirmation'
         });
@@ -47,7 +47,7 @@ $(document).ready(function() {
 
     $('#example3').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('We can set as many buttons as we want and we handle the user\'s choice though the callback function attached to the <strong>onClose</strong> event.<br><br>See the next example to handle user\'s choice in a different way.', {
+        new $.Zebra_Dialog('We can set as many buttons as we want and we handle the user\'s choice though the callback function attached to the <strong>onClose</strong> event.<br><br>See the next example to handle user\'s choice in a different way.', {
             type: 'question',
             title: 'Custom buttons',
             buttons: ['Yes', 'No', 'Help'],
@@ -59,7 +59,7 @@ $(document).ready(function() {
 
     $('#example4').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('We can set as many buttons as we want and we can handle the user\'s choice though the callback functions attached to the buttons.', {
+        new $.Zebra_Dialog('We can set as many buttons as we want and we can handle the user\'s choice though the callback functions attached to the buttons.', {
             type: 'question',
             title: 'Custom buttons',
             buttons: [
@@ -72,7 +72,7 @@ $(document).ready(function() {
 
     $('#example5_1').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am positioned in the <strong>top-left</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'left + 20\', \'top + 20\']</code>', {
+        new $.Zebra_Dialog('I am positioned in the <strong>top-left</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'left + 20\', \'top + 20\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['left + 20', 'top + 20']
@@ -81,7 +81,7 @@ $(document).ready(function() {
 
     $('#example5_2').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am positioned in the <strong>top-right</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'right - 20\', \'top + 20\']</code>', {
+        new $.Zebra_Dialog('I am positioned in the <strong>top-right</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'right - 20\', \'top + 20\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['right - 20', 'top + 20']
@@ -90,7 +90,7 @@ $(document).ready(function() {
 
     $('#example5_3').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am positioned in the <strong>bottom-right</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'right - 20\', \'bottom - 20\']</code>', {
+        new $.Zebra_Dialog('I am positioned in the <strong>bottom-right</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'right - 20\', \'bottom - 20\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['right - 20', 'bottom - 20']
@@ -99,7 +99,7 @@ $(document).ready(function() {
 
     $('#example5_4').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am positioned in the <strong>bottom-left</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'left + 20\', \'bottom - 20\']</code>', {
+        new $.Zebra_Dialog('I am positioned in the <strong>bottom-left</strong> corner, 20&nbsp;pixels from the edges. Here\'s how it\'s done:<br><code>position: [\'left + 20\', \'bottom - 20\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['left + 20', 'bottom - 20']
@@ -108,7 +108,7 @@ $(document).ready(function() {
 
     $('#example5_5').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am positioned in the <strong>center</strong> of the screen. Here\'s how it\'s done:<br><code>position: [\'center\', \'center\']</code>', {
+        new $.Zebra_Dialog('I am positioned in the <strong>center</strong> of the screen. Here\'s how it\'s done:<br><code>position: [\'center\', \'center\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['center', 'center']
@@ -117,7 +117,7 @@ $(document).ready(function() {
 
     $('#example5_6').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am centered horizontally and 50&nbsp;pixels from the top edge. Here\'s how it\'s done:<br><code>position: [\'center\', \'top + 50\']</code>', {
+        new $.Zebra_Dialog('I am centered horizontally and 50&nbsp;pixels from the top edge. Here\'s how it\'s done:<br><code>position: [\'center\', \'top + 50\']</code>', {
             title: 'Custom positioning',
             width: 460,
             position: ['center', 'top + 50']
@@ -126,7 +126,7 @@ $(document).ready(function() {
 
     $('#example6').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog('I am a notification widget. No buttons, no overlay, I am positioned in the top-right corner and I stay on screen for 2 seconds.', {
+        new $.Zebra_Dialog('I am a notification widget. No buttons, no overlay, I am positioned in the top-right corner and I stay on screen for 2 seconds.', {
             buttons: false,
             modal: false,
             position: ['right - 20', 'top + 20'],
@@ -147,7 +147,7 @@ $(document).ready(function() {
 
     $('#example8').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog({
+        new $.Zebra_Dialog({
             source: {
                 ajax: 'ajax.html'
             },
@@ -158,7 +158,7 @@ $(document).ready(function() {
 
     $('#example9').on('click', function(e) {
         e.preventDefault();
-        $.Zebra_Dialog({
+        new $.Zebra_Dialog({
             source: {
                 iframe: {
                     src: 'http://en.m.wikipedia.org/wiki/Dialog_box',
